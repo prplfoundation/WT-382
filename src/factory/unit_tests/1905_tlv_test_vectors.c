e26d4134db4d0dbcf03da6ed159d9627354fccfc
Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
2018-07-03 11:57:45 +0200

    tlv_test_vectors: include header and fix types
    1905_tlv_test_vectors.h was not included by 1905_tlv_test_vectors.c. As
    such, it went undetected that some of the types were wrong.

--- src/factory/unit_tests/1905_tlv_test_vectors.c
+++ src/factory/unit_tests/1905_tlv_test_vectors.c
@@ -84,6 +84,7 @@
 //   B) tlv = parse_1905_TLV_from_packet(stream_xxx);
 //
 
+#include "1905_tlv_test_vectors.h"
 
 ////////////////////////////////////////////////////////////////////////////////
 //// Test vector 001 (TLV <--> packet)
@@ -867,7 +868,7 @@ INT16U x1905_tlv_stream_len_023 = 4;
 ////
 ////////////////////////////////////////////////////////////////////////////////
 
-struct searchedRoleTLV x1905_tlv_structure_024 =
+struct supportedRoleTLV x1905_tlv_structure_024 =
 {
     .tlv_type                    = TLV_TYPE_SUPPORTED_ROLE,
     .role                        = IEEE80211_ROLE_REGISTRAR,
@@ -889,7 +890,7 @@ INT16U x1905_tlv_stream_len_024 = 4;
 ////
 ////////////////////////////////////////////////////////////////////////////////
 
-struct searchedRoleTLV x1905_tlv_structure_025 =
+struct supportedRoleTLV x1905_tlv_structure_025 =
 {
     .tlv_type                    = TLV_TYPE_SUPPORTED_ROLE,
     .role                        = 0x02,
@@ -911,7 +912,7 @@ INT16U x1905_tlv_stream_len_025 = 4;
 ////
 ////////////////////////////////////////////////////////////////////////////////
 
-struct autoconfigFreqBandTLV x1905_tlv_structure_026 =
+struct supportedFreqBandTLV x1905_tlv_structure_026 =
 {
     .tlv_type                    = TLV_TYPE_SUPPORTED_FREQ_BAND,
     .freq_band                   = IEEE80211_FREQUENCY_BAND_5_GHZ,
@@ -933,7 +934,7 @@ INT16U x1905_tlv_stream_len_026 = 4;
 ////
 ////////////////////////////////////////////////////////////////////////////////
 
-struct autoconfigFreqBandTLV x1905_tlv_structure_027 =
+struct supportedFreqBandTLV x1905_tlv_structure_027 =
 {
     .tlv_type                    = TLV_TYPE_SUPPORTED_FREQ_BAND,
     .freq_band                   = 0x07,
